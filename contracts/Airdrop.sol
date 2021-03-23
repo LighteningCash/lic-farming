824c4847c8593e9b7c4185ec424356f7db9d1ca0
lightening-cash <lightening.cash@gmail.com>
2021-03-23 15:44:17 +0700

    airdrop claim contract

---
+++ contracts/Airdrop.sol
@@ -0,0 +1,81 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; // for WETH
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Airdrop is Ownable {
+    address [] public whitelist;
+    address [] public claimed;
+    mapping (address => bool) public claimerWhitelist;
+    mapping (address => bool) public claimerClaimed;
+    mapping (address => uint256) public claimerBalances;
+
+    uint256 public startBlock;
+    uint256 public endBlock;
+
+    event Claim(address token, address holder, uint256 amount);
+
+    constructor (uint256 start, uint256 end) public{
+        startBlock = start;
+        endBlock = end;
+    }
+
+    function newAddress(address guy, uint256 amount) public onlyOwner {
+        whitelist.push(guy);
+        claimerWhitelist[guy] = true;
+        claimerBalances[guy] = amount;
+    }
+
+    function newAddress(address[] memory guys, uint256[] memory amount) public onlyOwner {
+        for (uint i = 0; i < guys.length; i++) {
+            whitelist.push(guys[i]);
+            claimerWhitelist[guys[i]] = true;
+            claimerBalances[guys[i]] = amount[i];
+        }
+    }
+
+    function newAddress(address[] memory guys, uint256 amount) public onlyOwner {
+        for (uint i = 0; i < guys.length; i++) {
+            whitelist.push(guys[i]);
+            claimerWhitelist[guys[i]] = true;
+            claimerBalances[guys[i]] = amount;
+        }
+    }
+
+    function claimToken(address token) public {
+        require(claimerClaimed[msg.sender] == false);
+        require(claimerWhitelist[msg.sender]);
+        require(block.number >= startBlock);
+        require(block.number <= endBlock);
+        require(IERC20(token).balanceOf(address(this)) >= getBalanceCanClain(msg.sender));
+        uint256 amount = getBalanceCanClain(msg.sender);
+        IERC20(token).transfer(msg.sender, amount);
+        claimerClaimed[msg.sender] = true;
+        claimed.push(msg.sender);
+        emit Claim(token, msg.sender, amount);
+    }
+
+    function getBalanceCanClain(address guy) public view returns (uint256){
+        if (!claimerWhitelist[guy]) {
+            return 0;
+        }
+        if (claimerClaimed[guy]) {
+            return 0;
+        }
+        if (block.number < startBlock || block.number > endBlock) {
+            return 0;
+        }
+        return claimerBalances[guy];
+    }
+
+    function withdrawAll(address token) onlyOwner public {
+        require(block.number > endBlock);
+        IERC20(token).transfer(msg.sender, IERC20(token).balanceOf(address(this)));
+    }
+
+    function withdraw(address token, uint256 amount) onlyOwner public {
+        require(block.number > endBlock);
+        IERC20(token).transfer(msg.sender, amount);
+    }
+}
